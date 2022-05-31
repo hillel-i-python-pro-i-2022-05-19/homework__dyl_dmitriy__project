08e7f58f85ec8f82e0aef9bc7de9defaa55598b5
DylDmitriy <dyl.dmitriy@gmail.com>
2022-05-31 14:13:14 +0300

    add file 1

--- cart.py
+++ cart.py
@@ -2,17 +2,17 @@ from goods import Goods
 
 
 class CartItem(object):
-    # Корзина товаров
+    # shopping cart
     def __init__(self, goods, count):
         self.goods = goods
         self.count = count
 
     def __str__(self):
-        #  f является заполнителем для десятичного типа
+        #  f decimal type
         return '%s(￥%.2f)*%s' % (self.goods.name,
                                  self.goods.price, self.count)
 
-        # Рассчитать подытог продукта
+        # shopping carttotal goods
 
     def amout(self):
         return self.goods.price * self.count
@@ -20,7 +20,7 @@ class CartItem(object):
 
 if __name__ == '__main__':
     goods = Goods('Goods1', 2999, 100)
-    # Создайте объект продукта корзины покупок, вам нужно передать объект продукта
+    # product object
     item = CartItem(goods, 2)
     money = item.amout()
     print(money)
